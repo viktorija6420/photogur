733346bc6515b78bef24bc7143e514f6a6899041
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-08-29 15:32:42 -0400

    a form for adding new pictures done

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,4 +2,16 @@ class PicturesController < ApplicationController
   def index
     @pictures = Picture.all
   end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def new
+   @picture = Picture.new
+ end
+
+ def create
+   render text: "Received POST request to '/pictures' with the data URL: #{params}"
+ end
 end
