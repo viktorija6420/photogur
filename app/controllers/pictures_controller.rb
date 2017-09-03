220c9afcc8a28916d1995c5c60c8b5e2a443c49c
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-09-03 17:30:37 -0400

    added edit

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,21 +4,55 @@ class PicturesController < ApplicationController
   end
 
   def show
-    @picture = Picture.find(params[:id])
+      @picture = Picture.find(params[:id])
   end
 
-  def new
-   @picture = Picture.new
- end
 
- def create
-   render text: "Received POST request to '/pictures' with the data URL: #{params}"
- end
+def new
+  @picture = Picture.new
+end
 
- def destroy
+   def create
+     render text: "Received POST request to '/pictures' with the data URL: #{params}"
+      @picture = Picture.new
+
+      @picture.title = params[:picture][:title]
+      @picture.artist = params[:picture][:artist]
+      @picture.url = params[:picture][:url]
+
+
+      if @picture.save
+    # if the picture gets saved, generate a get request to "/pictures" (the index)
+        redirect_to "/pictures"
+      else
+     # otherwise render new.html.erb
+        render :new
+     end
+
+    end
+end
+    def edit
+        @picture = Picture.find(params[:id])
+      end
+
+      def update
+        @picture = Picture.find(params[:id])
+
+        @picture.title = params[:picture][:title]
+        @picture.artist = params[:picture][:artist]
+        @picture.url = params[:picture][:url]
+
+
+        if @picture.save
+          redirect_to "/pictures/#{@picture.id}"
+        else
+          render :edit
+        end
+      end
+
+  def destroy
     @picture = Picture.find(params[:id])
     @picture.destroy
     redirect_to "/pictures"
   end
-  
 end
