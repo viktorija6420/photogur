a01619b46a7c9aa3a0a9c1ae38a34664ad61fb1c
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-09-11 14:46:43 -0400

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'pictures#index'
-get 'pictures' => 'pictures#index'
+resources :pictures
+# get 'pictures' => 'pictures#index'
+#
+#
+# post 'pictures' => 'pictures#create'
+# get 'pictures/new' => 'pictures#new'
+#
+#
+# get 'pictures/:id/edit' => "pictures#edit"
+# patch 'pictures/:id' => "pictures#update"
+#
+# get 'pictures/:id' => 'pictures#show'
+# delete 'pictures/:id' => 'pictures#destroy'
 
-
-post 'pictures' => 'pictures#create'
-get 'pictures/new' => 'pictures#new'
-
-
-get 'pictures/:id/edit' => "pictures#edit"
-patch 'pictures/:id' => "pictures#update"
-
-get 'pictures/:id' => 'pictures#show'
-delete 'pictures/:id' => 'pictures#destroy'
-
+get 'users' => 'pictures#index'
+post 'users' => 'users#create'
+get 'users/new' => 'users#new'
 end
